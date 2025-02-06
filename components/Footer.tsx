1a3074ac95ec3e5caca0609eef6bf1f7e6642c86
arfinorestualam <arfino.alam@gmail.com>
2025-02-06 08:20:29 +0700

    update things

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-6">
+    <footer className="bg-indigo-800 text-indigo-100 py-6">
       <div className="container mx-auto px-4 text-center">
         <p>&copy; 2025 Yu-Gi-Oh! Card Gallery. All rights reserved.</p>
-        <p className="mt-2 text-sm text-gray-400">
+        <p className="mt-2 text-sm text-indigo-300">
           Card data provided by{" "}
           <a href="https://db.ygoprodeck.com/api-guide/" className="underline hover:text-white">
             YGOPRODeck
